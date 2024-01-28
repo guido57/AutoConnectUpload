fd3a8a2cc9673f124e8cee935f6bec920cee5c14
guido <guido@dogsally.com>
2024-01-28 19:16:18 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -57,45 +57,6 @@ AutoConnectAux auxBrowse;
 AutoConnectAux auxPlay;
 AutoConnectAux auxDelete;
 
-#ifdef ARDUINO_ARCH_ESP32
-String getContentType(const String& filename) {
-  if (filename.endsWith(".txt")) {
-    return "text/plain";
-  } else if (filename.endsWith(".htm")) {
-    return "text/html";
-  } else if (filename.endsWith(".html")) {
-    return "text/html";
-  } else if (filename.endsWith(".css")) {
-    return "text/css";
-  } else if (filename.endsWith(".js")) {
-    return "application/javascript";
-  } else if (filename.endsWith(".json")) {
-    return "application/json";
-  } else if (filename.endsWith(".png")) {
-    return "image/png";
-  } else if (filename.endsWith(".gif")) {
-    return "image/gif";
-  } else if (filename.endsWith(".jpg")) {
-    return "image/jpeg";
-  } else if (filename.endsWith(".jpeg")) {
-    return "image/jpeg";
-  } else if (filename.endsWith(".ico")) {
-    return "image/x-icon";
-  } else if (filename.endsWith(".svg")) {
-    return "image/svg+xml";
-  } else if (filename.endsWith(".xml")) {
-    return "text/xml";
-  } else if (filename.endsWith(".pdf")) {
-    return "application/x-pdf";
-  } else if (filename.endsWith(".zip")) {
-    return "application/x-zip";
-  } else if (filename.endsWith(".gz")) {
-    return "application/x-gzip";
-  }
-  return "application/octet-stream";
-}
-#endif
-
 /* 
 Append all the filenames found in the SPIFFS root ("/") to the radio element 
 as RadioButtons
